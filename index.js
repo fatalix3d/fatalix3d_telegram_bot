43fab3636b8e16a338711c7380f66f75ba2bd050
fatalix3d <luckytima@gmail.com>
2023-03-25 02:09:43 +0600

    second update

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 
 const TelegramApi = require('node-telegram-bot-api');
-
 const token = '6000741658:AAFbDipcdGjFyGvSw1sza5UYIfAnPItNekI';
-
 const bot = new TelegramApi(token, {polling: true});
 
-const chats = {}
-
 bot.setMyCommands([
-    {command: '/start', description: 'Начало'},
-    {command: '/info', description: 'О боте'},
-    {command: '/crack', description: 'Взломать пентагон'},
-    {command: '/game', description: 'Сыграть на очко'}
+    {command: '/start', description: 'Запуск бота'},
+    {command: '/register', description: 'Регистрация'},
 ])
 
 const  start = () => {
-    bot.on('message', async msg =>{
+    let register = false;
+    let first_name = null;
+    let second_name = null;
+
+    bot.on('message', async msg => {
 
         //Вытаскием текст из сообщения
-        const  text = msg.text;
+        const text = msg.text;
         const chatId = msg.chat.id;
+        const userId = msg.chat.username;
+        console.log(userId);
 
-        //bot.sendMessage(chatId, `Ты написал мне ${text}`)
-        console.log(msg);
+        if(userId == 'ftx3d'){
+            await  bot.sendMessage(chatId, 'Админ в здании...');
+        }
 
-        if(text === '/start'){
-            return   bot.sendMessage(chatId, `${msg.chat.first_name} добро пожаловать в тестовый бот, пока он нечего не умеет, но быстро учится :) `)
+        if(register==false) {
+
+            if (text === '/start') {
+                return bot.sendMessage(chatId, `Добро пожаловать в тестовый бот, выберите /register для процедуры регистрации. Текущий статус регистрации [${register}]`)
+            }
+
+            if (text == '/register') {
+                register = true;
+                await  bot.sendMessage(chatId, 'Начало процедуры регистрации. Следуйте инструкции.')
+                return bot.sendMessage(chatId, `Регистрация, введите свое имя : `);
+            }
+
+            return bot.sendMessage(chatId, `Добро пожаловать в тестовый бот, выберите /register для процедуры регистрации Текущий статус регистрации [${register}]`)
         }
-        if(text == '/info'){
-            return   bot.sendMessage(chatId, `Это тестовый бот, нечего тут больше нет`)
+
+        if (first_name == null) {
+
+            //Проверка на цифры
+            if (containsDigits(msg.text)) {
+                return bot.sendMessage(chatId, `В вашем имени цифры? Попробуй еще раз`);
+            } else {
+                first_name = msg.text;
+                return bot.sendMessage(chatId, `Здравствуйте ${first_name}, теперь давай напиши фамилию : `);
+            }
+
         }
-        if(text == '/crack'){
-            return   bot.sendMessage(chatId, `Ваше очко взломано!`)
+
+        if (register == true && first_name != null && second_name == null) {
+            second_name = msg.text;
+
+            return bot.sendMessage(chatId, `${first_name} ${second_name}, вы зарегестрированы! Спасибо.`);
         }
-        if(text ==='/game'){
-            await bot.sendMessage(chatId, 'Я загадаю цифру от 0 до 9, а ты должен угадать! Иначе твое очко улетает в зал')
-            const randomNumber = Math.floor((Math.random() * 10))
-            chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Отгадывай сцуко')
-        }
-        return  bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз')
     });
+}
+
+function containsDigits(str) {
+    const regex = /\d/;
+    return regex.test(str);
 }
 
 start()
